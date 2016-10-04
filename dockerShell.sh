66f55701507c346d7f94dd934b809d633d6cfdcb
Windham Wong <windhamwong@nva-hk.com>
2016-10-04 15:25:16 +0100

    Update dockerShell.sh
    Adding check for ARGS

--- dockerShell.sh
+++ dockerShell.sh
@@ -1,7 +1,12 @@
-echo -e "\e[93m[+] Docker Shell Script\e[97m"
-docker ps
-echo -e "\e[92m[*] Which docker do you want to attach? \e[97m"
-read dockerName
+if [ $# -eq 0 ]
+        then
+        echo -e "\e[93m[+] Docker Shell Script\e[97m"
+        docker ps
+        echo -e "\e[92m[*] Which docker do you want to attach? \e[97m"
+        read dockerName
+else
+        dockerName=$1
+fi
 echo -e "\e[92m[*] You are now entering $dockerName...\e[97m"
 echo ""
 docker exec -i -t $dockerName /bin/bash
