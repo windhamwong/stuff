60d4473cc7a327c8a5b8a89a6d66bf19bafc96ad
Windham Wong <windhamwong@nva-hk.com>
2017-11-21 08:08:23 +0000

    adding an automated installation script

---
+++ auto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+print "[+] Automated installation script"
+
+print "[+] Installing new bash interface..."
+wget https://raw.githubusercontent.com/windhamwong/stuff/master/.bashrc -O ~/.bashrc
+
+print "[+] installing byobu..."
+sudo apt-get install byobu -y
+mkdir ~/.byobu
+rm -rf ~/.byobu/.tmux.conf ~/.byobu/status
+wget https://raw.githubusercontent.com/windhamwong/stuff/master/.byobu/.tmux.conf -O ~/.byobu/.tmux.conf
+wget https://raw.githubusercontent.com/windhamwong/stuff/master/.byobu/status -O ~/.byobu/status
+
